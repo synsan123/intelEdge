8e8a2d299b8f2669dfea8be8da17cdc4f3ba5f19
synsan123 <68645250+synsan123@users.noreply.github.com>
2023-12-21 14:36:36 +0900

    Add files via upload

---
+++ 1221/me.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "정보전자공학\n";
+	std::cout << "28세\n";
+	std::cout << "AI Engineer\n";
+	return 0;
+}
---
+++ 1221/name.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "My name is Kim Yong Je.\n";
+	return 0;
+}
